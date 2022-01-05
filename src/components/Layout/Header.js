731f97eea7c8d08dd4bced1463bf74a4cd3213a5
John Bryce <johnathanbryce@gmail.com>
2022-01-05 10:23:10 -0800

    Added new branch test feature

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import classes from "./Header.module.css";
 import { NavLink } from "react-router-dom";
 
+console.log("new feature to test git branch");
+
 const Header = () => {
   return (
     <div className={classes.header}>
